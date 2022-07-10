9d4d67440b979b95c8b4cfb6abbe12c63530a82c
priyankashambhanagoudar <priyassss789@gmail.com>
2022-07-10 12:09:38 +0530

    created order api

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv'
 import seedRouter from "./routes/seedRoutes.js"
 import productRouter from "./routes/productRoutes.js"
 import userRouter from './routes/userRoutes.js';
-
+import orderRouter from './routes/orderRoutes.js';
 dotenv.config()
 
 
@@ -28,6 +28,7 @@ app.use('/api/products', productRouter);
 
 
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 // test
 app.get('/api/products', (req, res) => {
